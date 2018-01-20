1f8eec55db96efd1aa05cc0339fc5deb57ad0d4b
Wyatt Kirby <wyatt@apsis.io>
2018-01-19 17:19:03 -0800

    Rubocop...

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,12 +1,12 @@
 if Rails.env.development?
   SecureHeaders::Configuration.default do |config|
     config.csp = config.csp.merge(
-      script_src: %w('self' 'unsafe-eval'),
-      style_src: %w('self' 'unsafe-inline')
+      script_src: ["'self'", "'unsafe-eval'"],
+      style_src: ["'self'", "'unsafe-inline'"]
     )
 
     config.csp_report_only = config.csp.merge(
-      report_uri: %w(https://apsis.io/)
+      report_uri: ['https://apsis.io/']
     )
   end
 else
