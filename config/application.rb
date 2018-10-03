fb66e6cc2bb1826b6e6249be371f325e7989cfe9
Eric Greer <eric@apsis.io>
2018-10-03 17:20:20 -0400

    updating rails to 5.2

--- config/application.rb
+++ config/application.rb
@@ -14,9 +14,9 @@ module RailsStarter
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.middleware.use Rack::Attack
     config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
 
