f8610d56ce2daf89dd9f013180429d14599b2f79
Eric Greer <eric@apsis.io>
2018-10-03 17:32:46 -0400

    re-adding letter opener config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,15 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.perform_deliveries = true
+  # To configure these locally, set the ENV vars in `.env.development.local`
+  config.action_mailer.default_url_options = {
+    protocol: ENV.fetch('APP_PROTOCOL') { 'http' },
+    host: ENV.fetch('APP_HOST') { 'localhost' },
+    port: ENV.fetch('APP_PORT') { 3000 } # 3000 internally -> 3000 on host machine
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
