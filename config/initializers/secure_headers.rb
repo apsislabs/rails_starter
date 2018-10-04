2751862f68b9d4f07455706182cdaec81b5c5b5e
Eric Greer <eric@apsis.io>
2018-10-04 11:30:54 -0400

    Adding secure headers config provided by chris

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -2,13 +2,29 @@
 
 if Rails.env.development?
   SecureHeaders::Configuration.default do |config|
-    config.csp = config.csp.merge(
-      script_src: ["'self'", "'unsafe-eval'"],
-      style_src: ["'self'", "'unsafe-inline'"]
-    )
+    config.cookies = {
+      secure: true, # mark all cookies as "Secure"
+      httponly: true, # mark all cookies as "HttpOnly"
+      samesite: {
+        lax: true # mark all cookies as SameSite=lax
+      }
+    }
 
-    config.csp_report_only = config.csp.merge(
-      report_uri: ['https://apsis.io/']
+    config.hsts = "max-age=#{1.week.to_i}"
+    config.x_frame_options = 'SAMEORIGIN'
+    config.x_content_type_options = 'nosniff'
+    config.x_xss_protection = '1; mode=block'
+    config.x_download_options = 'noopen'
+    config.x_permitted_cross_domain_policies = 'none'
+    config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
+
+    config.csp = config.csp.merge(
+      # directive values: these values will directly translate into source directives
+      default_src: %w('none'),
+      connect_src: %w('self'),
+      script_src: %w('self' 'unsafe-inline' 'unsafe-eval' http: https:),
+      font_src: %w(http: https:),
+      style_src: %w('self' 'unsafe-inline' http: https:),
     )
   end
 else
