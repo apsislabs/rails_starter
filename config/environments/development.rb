982dd8e47d5ad55ff3a3036c4e42463aa30523f7
Eric Greer <eric@apsis.io>
2019-10-17 17:39:38 -0400

    on development allowing every host so that you can set custom hosts. (useful for docker-toolbox)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # On development allow evey host through.
+  config.hosts.clear
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
