743373ba3e8c17810f4e49eeb1b2a41bd778b256
Eric Greer <eric@apsis.io>
2025-09-24 15:27:56 -0400

    add executable permissions

--- docker/start.sh
+++ docker/start.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
 # Wait for PostgreSQL
-until curl postgres:5432 2>&1 | grep '52'; do
+until curl ${POSTGRES_URL:-postgres}:5432 2>&1 | grep '52'; do
   echo 'Waiting for PostgreSQL...'
   sleep 1
 done
 echo "PostgreSQL is up and running"
-
-# Run Setup
-bin/setup
 
 # Run migrations and build assets in production
 if [ "$RAILS_ENV" = "production" ]; then
@@ -16,6 +13,11 @@ if [ "$RAILS_ENV" = "production" ]; then
   bin/rails db:migrate
 fi
 
+# Precompile assets
+echo "Precompiling Assets..."
+bundle exec rails assets:precompile
+
 # Delete the pidfile and start rails
+echo "Stating Rails..."
 rm -f tmp/pids/server.pid 2>/dev/null
 bin/rails s -b 0.0.0.0
