591b437bf2dcf1b434c8d163b0f91445918e9190
Wyatt Kirby <wyatt@apsis.io>
2026-05-28 22:58:22 -0400

    fix: fix incorrect include

--- config/initializers/extensions.rb
+++ config/initializers/extensions.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "data_uri"
-require "json_date_parser"
 
 # Load all extension files
 Dir[Rails.root.join("lib/*_extensions.rb").to_s].each { |file| require file }
