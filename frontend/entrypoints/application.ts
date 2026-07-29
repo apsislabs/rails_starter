26c135c09dcc12976f05fc199a396e8e2ca3c8e4
Wyatt Kirby <wyatt@apsis.io>
2026-07-29 11:34:51 -0400

    feat: add Rails UJS

--- frontend/entrypoints/application.ts
+++ frontend/entrypoints/application.ts
@@ -1,9 +1,14 @@
 import "./application.css";
 
 import { Application } from "@hotwired/stimulus";
+// @rails/ujs exposes a default export through its ESM build.
+// eslint-disable-next-line import-x/default
+import Rails from "@rails/ujs";
 import { initDidact } from "../apps/init";
 import { initStimulus } from "../controllers/init";
 import { Didact } from "../lib/Didact";
+
+Rails.start();
 
 declare global {
   interface Window {
